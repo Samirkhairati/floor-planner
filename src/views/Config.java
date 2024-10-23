b7176928de360cf6f2bd1437a7bb511bc2f5cfb2
Samir Khairati <samirk10101@gmail.com>
2024-10-24 01:02:00 +0530

    feat: added config file for constants in /views

---
+++ src/views/Config.java
@@ -0,0 +1,30 @@
+package views;
+public enum Config {
+    INSTANCE;
+
+    public static final int DEFAULT_ROOM_WIDTH = 100;
+    public static final int DEFAULT_ROOM_HEIGHT = 100;
+    public static final int DEFAULT_DOOR_WIDTH = 50;
+    public static final int DEFAULT_DOOR_HEIGHT = 10;
+    public static final int DEFAULT_WINDOW_WIDTH = 30;
+    public static final int DEFAULT_WINDOW_HEIGHT = 30;
+    public static final int DEFAULT_WALL_WIDTH = 10;
+    public static final int DEFAULT_WALL_HEIGHT = 10;
+    public static final int DEFAULT_FLOOR_WIDTH = 800;
+    public static final int DEFAULT_FLOOR_HEIGHT = 600;
+    public static final int DEFAULT_FLOOR_X = 0;
+    public static final int DEFAULT_FLOOR_Y = 0;
+    public static final int DEFAULT_ROOM_X = 0;
+    public static final int DEFAULT_ROOM_Y = 0;
+    public static final int DEFAULT_DOOR_X = 0;
+    public static final int DEFAULT_DOOR_Y = 0;
+    public static final int DEFAULT_WINDOW_X = 0;
+    public static final int DEFAULT_WINDOW_Y = 0;
+    public static final int DEFAULT_WALL_X = 0;
+    public static final int DEFAULT_WALL_Y = 0;
+    public static final int DEFAULT_WALL_COLOR = 0x000000;
+    public static final int DEFAULT_FLOOR_COLOR = 0xFFFFFF;
+    public static final int DEFAULT_ROOM_COLOR = 0x00FF00;
+    public static final int DEFAULT_DOOR_COLOR = 0x0000FF;
+    public static final int DEFAULT_WINDOW_COLOR = 0xFFFF00;
+}
