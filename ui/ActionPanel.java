3bdb19dbcbd7ae4e3c357790b8fbdc6bd44189cb
Samir Khairati <samirk10101@gmail.com>
2024-11-23 03:27:39 +0530

    chore: removed useless action panel buttons

--- ui/ActionPanel.java
+++ ui/ActionPanel.java
@@ -22,25 +22,17 @@ public class ActionPanel extends JPanel {
         setLayout(new WrapLayout());
 
         JButton button1 = new JButton("📦");
-        JButton button2 = new JButton("🚪");
-        JButton button3 = new JButton("🪟");
         JButton button4 = new JButton("💾");
         JButton button5 = new JButton("📁");
         Font font = new Font("Arial", Font.PLAIN, 30);
         button1.setFont(font);
-        button2.setFont(font);
-        button3.setFont(font);
         button4.setFont(font);
         button5.setFont(font);
         Dimension buttonSize = new Dimension(80, 50);
         button1.setPreferredSize(buttonSize);
-        button2.setPreferredSize(buttonSize);
-        button3.setPreferredSize(buttonSize);
         button4.setPreferredSize(buttonSize);
         button5.setPreferredSize(buttonSize);
         add(button1);
-        add(button2);
-        add(button3);
         add(button4);
         add(button5);
 
